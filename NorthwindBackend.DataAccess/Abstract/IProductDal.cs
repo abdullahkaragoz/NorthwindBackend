8be94684c86ca135ecc312a20928be3187b04ef5
abdul <abdullahkaragoz30@gmail.com>
2023-09-06 17:00:37 +0300

    update

---
+++ NorthwindBackend.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using NorthwindBackend.Core.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace NorthwindBackend.DataAccess.Abstract
+{
+    public class IProductDal 
+    {
+        
+    }
+}

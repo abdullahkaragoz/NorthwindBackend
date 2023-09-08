b82077de4e083b00eeb1db097452999d7ff144aa
abdul <abdullahkaragoz30@gmail.com>
2023-09-09 00:31:02 +0300

    -Added Category Entity

--- NorthwindBackend.Business/Constants/Messages.cs
+++ NorthwindBackend.Business/Constants/Messages.cs
@@ -6,8 +6,13 @@ namespace NorthwindBackend.Business.Constants
 {
     public static class Messages
     {
-        public static string ProductAdded = "Ürün başarıyla eklendi";
+        public static string ProductAdded   = "Ürün başarıyla eklendi";
         public static string ProductUpdated = "Ürün başarıyla güncellendi";
         public static string ProductDeleted = "Ürün başarıyla silindi";
+
+
+        public static string CategoryAdded   = "Kategori başarıyla eklendi";
+        public static string CategoryUpdated = "Kategori başarıyla güncellendi";
+        public static string CategoryDeleted = "Kategori başarıyla silindi";
     }
 }
---
+++ NorthwindBackend.DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,11 @@
+﻿using NorthwindBackend.Core.DataAccess.EntityFramework;
+using NorthwindBackend.DataAccess.Abstract;
+using NorthwindBackend.DataAccess.Concrete.EntityFramework.Contexts;
+using NorthwindBackend.Entities.Concrete;
+
+namespace NorthwindBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryDal : EfEntityRepositoryBase<Category,NorthwindContext>, ICategoryDal
+    {
+    }
+}

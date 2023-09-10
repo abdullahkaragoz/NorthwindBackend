5a7c52340cdaa6c0b1c81b73c9bc23bb1aad8ad2
Abdullah Karagöz <abdullah.karagoz@outlook.com>
2023-09-10 16:36:38 +0300

    #Auth implementations

---
+++ NorthwindBackend.Business/Abstract/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using NorthwindBackend.Core.Entities.Concrete;
+using NorthwindBackend.Core.Utilities.Results;
+using NorthwindBackend.Core.Utilities.Security.Jwt;
+using NorthwindBackend.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindBackend.Business.Abstract
+{
+    public interface IAuthService
+    {
+        IDataResult<User> Register(UserForRegisterDto userForRegister, string password);
+        IDataResult<User> Register(UserForRegisterDto userForRegister);
+        IDataResult<User> Login(UserForLoginDto userForLogin);
+        IResult UserExists(string email);
+        IDataResult<AccessToken> CreateAccessToken(User user);
+    }
+}
